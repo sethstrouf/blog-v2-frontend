4ed15e2e0d6bd5d6557bc09bbdf28613ebd34d1b
sethstrouf <seth.strouf@doane.edu>
2023-06-12 21:10:00 -0500

    Update api url with env variable

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,7 +1,8 @@
 import type { RequestHandler } from './$types'
+import { PUBLIC_API_HOST } from '$env/static/public';
 
 export const GET: RequestHandler = async () => {
-  const res = await fetch('http://localhost:3000/posts')
+  const res = await fetch(`${PUBLIC_API_HOST}/posts`)
   const data = await res.json()
 
   return new Response(JSON.stringify(data))
--- src/routes/api/posts/[postId]/+server.ts
+++ src/routes/api/posts/[postId]/+server.ts
@@ -1,7 +1,8 @@
 import type { RequestHandler } from './$types';
+import { PUBLIC_API_HOST } from '$env/static/public';
 
 export const GET: RequestHandler = async ({ params }) => {
-    const res = await fetch(`http://localhost:3000/posts/${params.postId}`)
+    const res = await fetch(`${PUBLIC_API_HOST}/posts/${params.postId}`)
     const data = await res.json()
 
     return new Response(JSON.stringify(data))
