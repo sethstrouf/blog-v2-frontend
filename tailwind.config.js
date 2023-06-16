a9aab2bf0f3325ef2ac2789ee42d4644ac35c5ce
sethstrouf <seth.strouf@doane.edu>
2023-06-15 20:23:10 -0500

    Add custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'valencia': {
+          50: "#fef3f2",
+          100: '#fde8e6',
+          200: '#fad3d1',
+          300: '#f5b0ac',
+          400: '#ef807d',
+          500: '#e4504f',
+          600: '#d44046',
+          700: '#ae222b',
+          800: '#921f2a',
+          900: '#7d1e2a',
+          950: '#460b11',
+        },
+        'turquoise': {
+          50: "#f0fdfb",
+          100: '#cdfaf4',
+          200: '#9bf4eb',
+          300: '#61e7dd',
+          400: '#40d4ce',
+          500: '#17b5b0',
+          600: '#109191',
+          700: '#117474',
+          800: '#135a5c',
+          900: '#144c4d',
+          950: '#052c2e',
+        },
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
