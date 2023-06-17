67acc0ce28ba528ae1e8af4af3de87649c401429
sethstrouf <seth.strouf@doane.edu>
2023-06-17 11:16:19 -0500

    Add items parameter for posts pagination

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -3,8 +3,9 @@ import { PUBLIC_API_HOST } from '$env/static/public';
 
 export const GET: RequestHandler = async ({ url }) => {
   const page = url.searchParams.get('page') || 1
+  const items = 10
 
-  const res = await fetch(`${PUBLIC_API_HOST}/posts?page=${page}`)
+  const res = await fetch(`${PUBLIC_API_HOST}/posts?items=${items}&page=${page}`)
   const data = await res.json()
 
   return new Response(JSON.stringify(data))
--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -2,9 +2,10 @@ import type { PageLoad } from './$types'
 
 export const load = (async ({ fetch, url }) => {
   let page = url.searchParams.get('page') || 1
+  const items = 10
 
   const fetchPosts = async () => {
-    const res = await fetch(`/api/posts?page=${page}`)
+    const res = await fetch(`/api/posts?items=${items}&page=${page}`)
     const data = await res.json()
 
     return data
